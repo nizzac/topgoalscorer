7ca745a95c94041a01771bd6e189f7f5b83b976e
Neil <neil@impelling.co.uk>
2019-12-30 10:03:08 +0000

    Add app actions component

---
+++ client/components/app-actions.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="actions text-center">
+        <nuxt-link :to="{ name: 'player-add' }"
+          class="bg-black text-white rounded px-6 py-3">
+          Add Player
+        </nuxt-link>
+    </div>
+</template>
